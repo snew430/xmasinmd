7c1c08eb13657812fcfe63b4da2403075f906357
snew430 <seanpnew@hotmail.com>
2022-08-12 09:46:34 -0500

    changed paths

--- src/components/NoMatch/NoMatch.js
+++ src/components/NoMatch/NoMatch.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const NoMatch = () => {
-  return (
-    <div>NoMatch</div>
-  )
-}
+  return <div>404 Error</div>;
+};
 
-export default NoMatch
+export default NoMatch;
