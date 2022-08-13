1f8d98e09bdac044e8b93bd6c4d3e27021a245f4
snew430 <seanpnew@hotmail.com>
2022-08-13 10:15:33 -0500

    minor changes to contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Contact.css';
 
 const Contact = () => {
   return (
     <div>
-      <h3>Feel free to contact us for more info</h3>
+      <h3 className="contact-title">Feel free to contact us for more info</h3>
       <ul>
-        <li>Email</li>
-        <li>Twitter</li>
-        <li>FaceBook</li>
+        <li className="contact-item">Email</li>
+        <li className="contact-item">Twitter</li>
+        <li className="contact-item">FaceBook</li>
       </ul>
     </div>
   );
