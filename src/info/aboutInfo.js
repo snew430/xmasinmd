8798428d6ef793e4bc0186d5f25a004611c0d1d2
snew430 <seanpnew@hotmail.com>
2022-08-13 00:10:46 -0500

    changed placeholders

--- src/info/aboutInfo.js
+++ src/info/aboutInfo.js
@@ -1,13 +1,13 @@
 const info = [
   {
     title: 'Your Host: John Patti',
-    pic: 'https://www.placecage.com/200/200',
+    pic: 'https://www.placecage.com/250/270',
     bio: 'Hi, Award winning journalist and longtime Baltimore broadcaster John Patti is your host for Christmas in Maryland. John takes his microphone across the state to bring you cherished holiday memories, and the Christmas music you grew up with.',
     link: '',
   },
   {
     title: 'Joe Evelius',
-    pic: 'https://www.placecage.com/100/100',
+    pic: 'https://www.placecage.com/220/300',
     bio: 'Veteran Maryland broadcaster Joe Evelius is the co-founder, producer and engineer responsible for the sound of Christmas in Maryland. He also serves as our Music Director.',
     link: 'https://www.facebook.com/AudioSolutions.Restoration.Conversion.Production',
   },
