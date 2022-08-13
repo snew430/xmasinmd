ad2e3c919918954e61f89fef8787f5deeb2c3585
snew430 <seanpnew@hotmail.com>
2022-08-12 23:40:50 -0500

    fixed about overflow

--- src/components/AboutCard/AboutCard.js
+++ src/components/AboutCard/AboutCard.js
@@ -9,13 +9,14 @@ const AboutCard = ({ person }) => {
       <Card className="card" style={{ width: '18rem' }}>
         <Card.Img className="card-image" variant="top" src={pic} />
         <Card.Body>
-          <Card.Title>{title}</Card.Title>
+          <Card.Title>
+            <a href={link} target="_blank">
+              {title}
+            </a>
+          </Card.Title>
         </Card.Body>
       </Card>
-      <p className="bio-text">
-        {bio}
-        {link}
-      </p>
+      <p className="bio-text">{bio}</p>
     </div>
   );
 };
