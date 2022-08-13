d830016abf5ebba0c8083a0b3df8b6d10aa88635
snew430 <seanpnew@hotmail.com>
2022-08-13 10:08:41 -0500

    changed sponsor page

--- src/components/Stations/Stations.js
+++ src/components/Stations/Stations.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import stations from '../../info/stationInfo';
+import './Stations.css';
 
 const Stations = () => {
   return (
-    <div>Stations</div>
-  )
-}
+    <>
+      <h3 className="tune">Stations to Tune Into...</h3>
+      {stations.map(({ name, location, freq, dates }) => (
+        <div key={name} className="station-card">
+          <h5>{name}</h5>
+          <p>
+            ({location}) - {freq}
+          </p>
+          <p>
+            Dates
+            {dates.map((date) => (
+              <p key={date}>{date}</p>
+            ))}
+          </p>
+        </div>
+      ))}
+    </>
+  );
+};
 
-export default Stations
+export default Stations;
---
+++ src/info/stationInfo.js
@@ -0,0 +1,34 @@
+const stations = [
+  {
+    name: 'WCBC',
+    location: 'Cumberland, MD',
+    freq: 'AM 1270',
+    dates: ['Dec. 24 4p - Midnight', 'Dec. 25 Midnight - 8pm'],
+  },
+  {
+    name: 'WCEI-FM',
+    location: 'Easton, MD',
+    freq: 'FM 96.7',
+    dates: ['Dec. 24 9p - Midnight', 'Dec. 25 12a - 8 and 9p - Midnight'],
+  },
+  {
+    name: 'WNIX-FM',
+    location: 'St. Michales, MD',
+    freq: 'FM 94.3',
+    dates: ['Dec. 24 12p - Midnight', 'Dec. 25 6a - 8p'],
+  },
+  {
+    name: 'WNAV-AM/FM',
+    location: 'Annapolis, MD',
+    freq: 'AM 1430',
+    dates: ['Dec. 11 - 24 11a - Midnight', 'Dec. 25 10a - Midnight'],
+  },
+  {
+    name: 'WCTR-AM',
+    location: 'Annapolis, MD',
+    freq: 'AM 1530',
+    dates: ['Dec. 18 - 26 2p - 4p'],
+  },
+];
+
+module.exports = stations;
