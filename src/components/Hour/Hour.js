869ba83c871a168775ba7908e47f3afe251842b2
Reid Schroder <reidschroder2@gmail.com>
2022-09-29 14:38:39 -0400

    initial takeover commit

--- src/components/Hour/Hour.js
+++ src/components/Hour/Hour.js
@@ -12,43 +12,43 @@ const Hour = () => {
     setCurrentHour(hour);
   };
 
-  return <div>Show Schedule Coming Soon</div>;
+  // return <div>Show Schedule Coming Soon</div>;
 
-  // return (
-  //   <div className="hour-info">
-  //     <div className="hour-buttons">
-  //       <ButtonGroup aria-label="Basic example">
-  //         <Button onClick={(e) => changeHour(e, hour1)} variant="success">
-  //           Hour 1
-  //         </Button>
-  //         <Button onClick={(e) => changeHour(e, hour2)} variant="success">
-  //           Hour 2
-  //         </Button>
-  //         <Button onClick={(e) => changeHour(e, hour3)} variant="success">
-  //           Hour 3
-  //         </Button>
-  //         <Button onClick={(e) => changeHour(e, hour4)} variant="success">
-  //           Hour 4
-  //         </Button>
-  //         <Button onClick={(e) => changeHour(e, hour5)} variant="success">
-  //           Hour 5
-  //         </Button>
-  //         <Button onClick={(e) => changeHour(e, hour6)} variant="success">
-  //           Hour 6
-  //         </Button>
-  //       </ButtonGroup>
-  //     </div>
-  //     <h3 id="hourHeader">Hour 1</h3>
-  //     <section className="hour-section">
-  //       {currentHour.map((hourcontent) => (
-  //         <>
-  //           <p>{hourcontent}</p>
-  //           <hr />
-  //         </>
-  //       ))}
-  //     </section>
-  //   </div>
-  // );
+  return (
+    <div className="hour-info">
+      <div className="hour-buttons">
+        <ButtonGroup aria-label="Basic example">
+          <Button onClick={(e) => changeHour(e, hour1)} variant="success">
+            Hour 1
+          </Button>
+          <Button onClick={(e) => changeHour(e, hour2)} variant="success">
+            Hour 2
+          </Button>
+          <Button onClick={(e) => changeHour(e, hour3)} variant="success">
+            Hour 3
+          </Button>
+          <Button onClick={(e) => changeHour(e, hour4)} variant="success">
+            Hour 4
+          </Button>
+          <Button onClick={(e) => changeHour(e, hour5)} variant="success">
+            Hour 5
+          </Button>
+          <Button onClick={(e) => changeHour(e, hour6)} variant="success">
+            Hour 6
+          </Button>
+        </ButtonGroup>
+      </div>
+      <h3 id="hourHeader">Hour 1</h3>
+      <section className="hour-section">
+        {currentHour.map((hourcontent) => (
+          <>
+            <p>{hourcontent}</p>
+            <hr />
+          </>
+        ))}
+      </section>
+    </div>
+  );
 };
 
 export default Hour;
