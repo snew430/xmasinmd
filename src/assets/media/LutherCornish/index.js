832316ede1d9bff1ad9d485543c64f45d955e03c
snew430 <seanpnew@hotmail.com>
2022-11-30 17:20:00 -0500

    removed LC video

--- src/assets/media/LutherCornish/index.js
+++ src/assets/media/LutherCornish/index.js
@@ -1,10 +1,11 @@
 import pic1 from './LutherCornish.jpg';
 import audio1 from './LutherCornish.mp3';
+// import video1 from './LutherCornish.mp4'
 
 const lutherCornish = {
   name: 'Luther Cornish',
   photos: [ pic1 ],
-  videos: [],
+  // videos: [ video1 ],
   audio: [ audio1 ],
 };
 
