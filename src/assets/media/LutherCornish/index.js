2e706da6b870ff5cd5adbf14496cfb9ec3dadde4
snew430 <seanpnew@hotmail.com>
2022-11-30 17:20:32 -0500

    added back LC video

--- src/assets/media/LutherCornish/index.js
+++ src/assets/media/LutherCornish/index.js
@@ -1,11 +1,11 @@
 import pic1 from './LutherCornish.jpg';
 import audio1 from './LutherCornish.mp3';
-// import video1 from './LutherCornish.mp4'
+import video1 from './LutherCornish.mp4'
 
 const lutherCornish = {
   name: 'Luther Cornish',
   photos: [ pic1 ],
-  // videos: [ video1 ],
+  videos: [ video1 ],
   audio: [ audio1 ],
 };
 
