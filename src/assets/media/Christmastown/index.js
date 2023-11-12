de15f63bf0952a67aeee35499060c85f5f4174d5
snew430 <seanpnew@hotmail.com>
2023-11-11 21:39:17 -0500

    some videos

--- src/assets/media/Christmastown/index.js
+++ src/assets/media/Christmastown/index.js
@@ -4,13 +4,19 @@ import pic3 from './Christmastown3.JPG';
 import pic4 from './Christmastown4.JPG';
 import pic5 from './Christmastown5.JPG';
 import audio1 from './Christmastown.MP3';
+import video1 from './Christmastown1.mp4';
+import video2 from './Christmastown2.mp4';
 
 const christmastown = {
   tab: 'ChristmasTown',
   name: 'CHRISTMAS TOWN – PITTSVILLE, MARYLAND',
-  description: 'Mike and Marsha Newton say “we live and breath Christmas.” Christmas Town helps local families with the donations received, bringing smiles and tears of joy to them. The Newtons have holiday displays illuminated from 5pm til 8pm weather permitting each evening during the holiday season at 6685 Friendship Road, Pittsville, Maryland. Please be safe when parking and please don\'t turn around in neighbors driveways .',
+  description:
+    "Mike and Marsha Newton say “we live and breath Christmas.” Christmas Town helps local families with the donations received, bringing smiles and tears of joy to them. The Newtons have holiday displays illuminated from 5pm til 8pm weather permitting each evening during the holiday season at 6685 Friendship Road, Pittsville, Maryland. Please be safe when parking and please don't turn around in neighbors driveways .",
   photos: [pic1, pic2, pic3, pic4, pic5],
-  videos: [],
+  videos: [
+    {video: video1, poster: pic1},
+    {video: video2, poster: pic2},
+  ],
   audio: [audio1],
 };
 
