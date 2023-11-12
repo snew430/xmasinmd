6f4d3ab82ad935af25b6040847074e78acd916bd
snew430 <seanpnew@hotmail.com>
2023-11-11 21:31:57 -0500

    some videos

--- src/assets/media/Schellville/index.js
+++ src/assets/media/Schellville/index.js
@@ -8,6 +8,9 @@ import pic7 from './Schellville7.jpeg';
 import pic8 from './Schellville8.JPG';
 import pic9 from './Schellville9.JPG';
 import audio1 from './Schellville.mp3';
+import video1 from './Schellville1.mp4';
+import video2 from './Schellville2.mp4';
+import video3 from './Schellville3.mp4';
 
 const schellville = {
   tab: 'Schellville',
@@ -15,7 +18,11 @@ const schellville = {
   description:
     'Want to have a snowball battle no matter what the weather? Experience a sledding hill in the snow? How about a train ride around the village, or a trip through the Christmas tree maze? Of course, everyone wants to visit Santa and Mrs. Claus in their Chalet! You can do it all every evening during the holiday season at Schellville. Listen as we take you on a special audio tour.',
   photos: [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9],
-  videos: [],
+  videos: [
+    {video: video1, poster: pic1},
+    {video: video2, poster: pic2},
+    {video: video3, poster: pic3},
+  ],
   audio: [audio1],
 };
 
